69b7a2959dff60b284600b86f60504f96e3ea6d7
Alvin <alvinmutisya@gmail.com>
2016-11-10 21:16:46 +0300

    The append_prime_number modified to add more restricitons based on the test_cases

--- prime_numbers.py
+++ prime_numbers.py
@@ -6,15 +6,23 @@ def prime_number(number):
             if number % value is 0:
                 return False
         return True
-def append_prime_numbers(my_number):
-    if  isinstance(my_number, int):
 
-        sto_prime = []
+def append_prime_numbers(my_number):
+    sto_prime = []
+    if isinstance(my_number, bool):
+        return "Operation cannot allow boolean value, integers only."
+    elif isinstance(my_number, str):
+        return "Operation cannot allow a string, integers only!"
+    elif isinstance(my_number, dict):
+        return "Operation cannot handle dictionaries, integers only"
+    elif isinstance(my_number, list):
+        return "Operation cannot handle a list, integers only"
+    elif my_number <= 0:
+        return "Prime numbers exist only for positive integers"
+    elif  isinstance(my_number, int):
         for item in range(2, my_number + 1):
             if prime_number(item):
                 sto_prime.append(item)
         return sto_prime
-    else:
-        return "Numbers only"
-        
-print append_prime_numbers(10)
+
+print append_prime_numbers(-20)

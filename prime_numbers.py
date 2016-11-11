bff20d1af91df0139547511f7798646110de4d52
Alvin <alvinmutisya@gmail.com>
2016-11-11 18:26:19 +0300

    Changed the prime append_prime_numbers function to work more efficiently

--- prime_numbers.py
+++ prime_numbers.py
@@ -8,21 +8,15 @@ def prime_number(number):
         return True
 
 def append_prime_numbers(my_number):
-    sto_prime = []
-    if isinstance(my_number, bool):
-        return "Operation cannot allow boolean value, integers only."
-    elif isinstance(my_number, str):
-        return "Operation cannot allow a string, integers only!"
-    elif isinstance(my_number, dict):
-        return "Operation cannot handle dictionaries, integers only"
-    elif isinstance(my_number, list):
-        return "Operation cannot handle a list, integers only"
-    elif my_number <= 0:
-        return "Prime numbers exist only for positive integers"
-    elif  isinstance(my_number, int):
-        for item in range(2, my_number + 1):
-            if prime_number(item):
-                sto_prime.append(item)
-        return sto_prime
+   sto_prime = []
+   if not isinstance(my_number, int):
+       return "Operation cannot allow " + str(type(my_number)) + ", integers only."
+   elif my_number <= 0:
+       return "Prime numbers exist only for positive integers"
+   elif  isinstance(my_number, int):
+       for item in range(2, my_number + 1):
+           if prime_number(item):
+               sto_prime.append(item)
+       return sto_prime
 
-print append_prime_numbers(-20)
+print append_prime_numbers([20, 30])
